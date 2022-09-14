7a3794080cc362f8b4b7541305f84c29901be679
lemmaandrew <lemmajandrew@gmail.com>
2022-09-14 17:11:57 -0500

    numX -> numXs

--- warmup2/CountXX.java
+++ warmup2/CountXX.java
@@ -1,11 +1,11 @@
 package warmup2;
 
 public class CountXX {
-    public static int countXXBaseline(String str, int numX) {
+    public static int countXXBaseline(String str, int numXs) {
         int count = 0;
-        String cmpStr = "x".repeat(numX);
-        for (int i = 0; i < str.length() - numX + 1; ++i) {
-            if (str.substring(i, i + numX).equals(cmpStr)) {
+        String cmpStr = "x".repeat(numXs);
+        for (int i = 0; i < str.length() - numXs + 1; ++i) {
+            if (str.substring(i, i + numXs).equals(cmpStr)) {
                 ++count;
             }
         }
@@ -16,19 +16,19 @@ public class CountXX {
         return countXXBaseline(str, 2);
     }
 
-    public static int countXX(String str, int numX) {
+    public static int countXX(String str, int numXs) {
         int count = 0;
-        for (int i = 0; i < str.length() - numX + 1; ++i) {
+        for (int i = 0; i < str.length() - numXs + 1; ++i) {
             // Save the current position
             int oldI = i;
             // Keep incrementing position while str.charAt(i) == 'x'
             for (; i < str.length() && str.charAt(i) == 'x'; ++i); 
-            // Increment count by how many positions past numX we got to
-            // So if numX == 2, oldI = 4, and i = 6, then including overlaps
+            // Increment count by how many positions past numXs we got to
+            // So if numXs == 2, oldI = 4, and i = 6, then including overlaps
             // we have 6 - 4 - 2 + 1 = 1 XX string of length two
-            // The + 1 is to offset the fact that, say, 6 - 4 == 2, but with numX == 2,
+            // The + 1 is to offset the fact that, say, 6 - 4 == 2, but with numXs == 2,
             // that would make for 0 additional XX strings, which isn't the case.
-            count += Math.max(i - oldI - numX + 1, 0);
+            count += Math.max(i - oldI - numXs + 1, 0);
         }
         return count;
     }
